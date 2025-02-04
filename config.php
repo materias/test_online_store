bd4d790a2230295f1a69478d4cddd3b31f6eff5e
Valeria <58702695+materias@users.noreply.github.com>
2025-02-04 19:42:57 +0400

    Set up basic DB configuration

--- config.php
+++ config.php
@@ -5,3 +5,15 @@ require __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+$host = $_ENV['DB_HOST'];
+$dbname = $_ENV['DB_NAME'];
+$username = $_ENV['DB_USERNAME'];;
+$password = $_ENV['DB_PASSWORD'];;
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Ошибка подключения к базе данных: " . $e->getMessage());
+}
+
