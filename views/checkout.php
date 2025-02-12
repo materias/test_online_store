bcb9238968d9397c8f00790f6427c7da9c55230e
Valeria <58702695+materias@users.noreply.github.com>
2025-02-12 15:59:24 +0400

    Eliminated redundant files

--- views/checkout.php
+++
@@ -1,9 +0,0 @@
-<head>
-    <link rel="stylesheet" href="../public/css/styles.css">
-</head>
-
-<script src="<?= $_ENV['PAYPAL_LINK']; ?>"></script>
-
-<div id="paypal-button-container"></div>
-
-<script src="public/js/paypal.js"></script>
